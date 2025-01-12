cf11e4c790436ac4d41beb3f8fdf4f4e6c2425ab
EkeHanson <ekenehanson@gmail.com>
2025-01-12 21:33:26 +0100

    qwertyui

--- core/settings.py
+++ core/settings.py
@@ -133,25 +133,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cmvp_db_105h',
-        'USER': 'cmvp_db_105h_user',
-        'PASSWORD': 'f4opFXGCPJYJHlbkcj66MKNOsAKJrs1B',
-        'HOST': 'dpg-cu13fnt2ng1s73e5ts20-a.oregon-postgres.render.com',
-        'PORT': '5432',  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'cmvp_db_105h',
+#         'USER': 'cmvp_db_105h_user',
+#         'PASSWORD': 'f4opFXGCPJYJHlbkcj66MKNOsAKJrs1B',
+#         'HOST': 'dpg-cu13fnt2ng1s73e5ts20-a.oregon-postgres.render.com',
+#         'PORT': '5432',  # Default PostgreSQL port
+#     }
+# }
 
 SIMPLE_JWT = {
 
