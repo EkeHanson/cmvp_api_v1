643d6481cf0035d8ff0ebb06dca6501d7f9c3193
YourGitHubUsername <EkeHanson>
2025-02-18 18:17:59 +0100

    yeah it is true

--- core/settings.py
+++ core/settings.py
@@ -248,8 +248,8 @@ logger.addHandler(logging.StreamHandler())
 
 
 
-# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'cmvp-files'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'eu-north-1'
